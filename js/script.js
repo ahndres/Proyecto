f6b02f30cbc83d34b082427d5568685e099696b7
Ahndres <ahndrs@yahoo.es>
2018-08-11 09:51:02 -0500

    trabajando
    javascript

---
+++ js/script.js
@@ -0,0 +1,18 @@
+var numero1 = 5;
+var numero2 = 10;
+alert (numero1+numero2);
+console.log(numero1)
+
+
+var numero = 1;
+var nombre = "mundo";
+var verdadero = false;
+
+//if else 
+
+if (verdadero) {
+    console.log("muerto")
+}
+else 
+console.log("hola")
+
